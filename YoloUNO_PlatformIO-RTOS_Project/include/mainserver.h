5462a1f13c9623d6733b38e0aaef0b3383336146
Truongle205 <141789870+Truongle205@users.noreply.github.com>
2025-12-03 14:21:04 +0700

    lab3

---
+++ YoloUNO_PlatformIO-RTOS_Project/include/mainserver.h
@@ -0,0 +1,30 @@
+#ifndef ___MAIN_SERVER__
+#define ___MAIN_SERVER__
+#include <Arduino.h>
+#include <WiFi.h>
+#include <WebServer.h>
+#include "global.h"
+
+#define LED1_PIN 48
+#define LED2_PIN 45
+#define BOOT_PIN 0
+//extern WebServer server;
+
+//extern bool isAPMode;
+
+extern String wifi_ssid;
+extern String wifi_password;
+extern String ssid;
+extern String password;
+
+
+String mainPage();
+String settingsPage();
+
+void startAP();
+void setupServer();
+void connectToWiFi();
+
+void main_server_task(void *pvParameters);
+
+#endif
